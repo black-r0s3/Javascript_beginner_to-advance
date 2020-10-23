ee5a9a6738d97eb93bcae9e85219d0905a690e54
black-r0s3 <ebenezer.nyarko@amalitech.org>
2020-10-23 23:10:22 +0800

    created a function to display the result of two numbers added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function addNumbers(num1, num2){
+    const result = num1 + num2;
+    alert('The result is ' + result);
+}
+
+addNumbers(4,3);
+addNumbers(2,9);
+
 currentResult = currentResult + (20 -5)  * 5;
 
 let calculationDescription = '(' +'currentResult' + '(20 -5)  * 5' +')';
