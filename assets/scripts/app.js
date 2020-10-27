489a5aa03d90072519c059f2122d434cf82de4fe
black-r0s3 <ebenezer.nyarko@amalitech.org>
2020-10-27 18:23:10 -0500

    added global variables as identiers in conditional statements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,9 @@ const MONSTER_ATTACK_VALUE = 12;
 const STRONG_ATTACK_VALUE = 15;
 const HEAL_VALUE = 18;
 
+const MODE_ATTACK = 'ATTACK'; 
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
+
 
 const enteredValue = prompt('Maximum life for you and the monster', '100')
 
@@ -50,9 +53,9 @@ function endRound(){
 
 function attackMonster(mode){
     let maxDamage;
-    if (mode === 'ATTACK'){
+    if (mode === MODE_ATTACK){
         maxDamage = ATTACK_VALUE;
-    } else if(mode=== "STRONG_ATTACK") {
+    } else if(mode=== MODE_STRONG_ATTACK) {
         maxDamage = STRONG_ATTACK_VALUE;
     }
     const damage = dealMonsterDamage(maxDamage);
@@ -61,11 +64,11 @@ function attackMonster(mode){
 }
 
 function attackHandler(){
-   attackMonster('ATTACK');
+   attackMonster(MODE_ATTACK);
 }
 
 function strongAttackHandler(){
-   attackMonster('STRONG_ATTACK');
+   attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler(){
