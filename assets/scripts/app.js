cc0121a053345cc1a86d0d71a490d23e121c948d
black-r0s3 <ebenezer.nyarko@amalitech.org>
2020-10-27 17:05:00 +0000

    added a heal functionality using if statement

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 12;
 const STRONG_ATTACK_VALUE = 15;
+const HEAL_VALUE = 18;
 
 
 let chosenMaxLife = 100;
@@ -9,15 +10,7 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMonster(mode){
-    let maxDamage;
-    if (mode === 'ATTACK'){
-        maxDamage = ATTACK_VALUE;
-    } else if(mode=== "STRONG_ATTACK") {
-        maxDamage = STRONG_ATTACK_VALUE;
-    }
-    const damage = dealMonsterDamage(maxDamage);
-    currentMonsterHealth -= damage;
+function endRound(){
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
     if (currentMonsterHealth <= 0 && currentPlayerHealth> 0){
@@ -29,6 +22,18 @@ function attackMonster(mode){
     }
 }
 
+function attackMonster(mode){
+    let maxDamage;
+    if (mode === 'ATTACK'){
+        maxDamage = ATTACK_VALUE;
+    } else if(mode=== "STRONG_ATTACK") {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
+    endRound();
+}
+
 function attackHandler(){
    attackMonster('ATTACK');
 }
@@ -37,5 +42,20 @@ function strongAttackHandler(){
    attackMonster('STRONG_ATTACK');
 }
 
+function healPlayerHandler(){
+    let healValue;
+    if  (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE){
+        alert("You can't heal more than your max initial health");
+        healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+        healValue = HEAL_VALUE;
+    }
+    increasePlayerHealth(HEAL_VALUE)
+    currentPlayerHealth += HEAL_VALUE;
+    endRound();
+}
+
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
